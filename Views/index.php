9fcbada7bf5cce512a6b3d971d23e2d945feff17
Veronic26 <thaliacampoverde26@gmail.com>
2025-02-24 22:07:26 -0500

    Add files via upload

---
+++ Views/index.php
@@ -0,0 +1,14 @@
+<?php include 'layouts/header.php'; ?>
+<div class="container">
+    <h1>Gestión de Libros y Autores</h1>
+    <p>Proyecto desarrollado por: </p>
+    <p>- Thalia Campoverde Maza</p>
+    <p>- Stefania Ojeda Rengel</p>
+    <p>- Gabriel Martínez Calahorrano</p>
+    <p>Esta aplicación permite gestionar libros y autores de manera eficiente.</p>
+    <!-- Corrección de los enlaces -->
+    <a href="<?php echo BASE_URL; ?>libros" class="btn btn-primary">Gestionar Libros</a>
+    <a href="<?php echo BASE_URL; ?>autores" class="btn btn-primary">Gestionar Autores</a>
+</div>
+<?php include 'layouts/footer.php'; ?>
+
