29f98d0f958c7e4fe149982add7266171d3fadb7
Veronic26 <thaliacampoverde26@gmail.com>
2025-05-12 16:24:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define('BASE_URL', '/gestion_libros_autores/');
 require_once 'router.php';
 
 $url = isset($_GET['url']) ? $_GET['url'] : '/';
-$router = old Router();
+$router = new Router();
 $router->route($url);
 
 
